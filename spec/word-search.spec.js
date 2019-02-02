3ff697414d3334b856d3749e0b6bbd761d435b7c
Derek Price <derek.price@dxc.com>
2019-02-02 09:17:08 -0500

    Ad test for backward searches.

--- spec/word-search.spec.js
+++ spec/word-search.spec.js
@@ -51,4 +51,12 @@ K,Y,L,B,Q,Q,P,M,D,F,C,K,E,V,B`;
         expect(app.search("CHEKOV\n" + haystack)).toBe('CHEKOV: (8,9),(9,10),(10,11),(11,12),(12,13),(13,14)\n');
     });
 
+    it('should search backwards', function () {
+        expect(app.search("KHAN,KIRK,SULU,UHURA\n" + haystack)).toBe(`KHAN: (5,9),(5,8),(5,7),(5,6)
+KIRK: (4,7),(3,7),(2,7),(1,7)
+SULU: (3,3),(2,2),(1,1),(0,0)
+UHURA: (4,0),(3,1),(2,2),(1,3),(0,4)
+`);
+    });
+
 });
